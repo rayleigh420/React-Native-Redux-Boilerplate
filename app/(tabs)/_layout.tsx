ce0b222a98f16c9cee686f4f1d623435b826b363
rayleigh420 <nhatduy0409@gmail.com>
2024-03-14 17:00:23 +0700

    Config Redux Toolkit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,7 +30,7 @@ export default function TabLayout() {
 			<Tabs.Screen
 				name="index"
 				options={{
-					title: "Demo",
+					title: "Todo",
 					tabBarIcon: ({ color }) => (
 						<TabBarIcon name="tasks" color={color} />
 					),
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,36 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+	const count = useSelector((state: RootState) => state.counter.value);
+
+	return (
+		// <View style={styles.container}>
+		//   <Text style={styles.title}>Tab One</Text>
+		//   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+		//   <EditScreenInfo path="app/(tabs)/index.tsx" />
+		// </View>
+		<Text>{count}</Text>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
 });
